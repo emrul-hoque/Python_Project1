26b69a047577e188ac0b838a4f82eed92c39ca0a
Emrul Hoque <108203835+emrul-hoque@users.noreply.github.com>
2023-07-06 20:03:32 +0600

    Add files via upload

---
+++ orange.py
@@ -0,0 +1,10 @@
+#For saying hello to the world
+print ("hello world")
+
+if 2 < 0.3299248:
+    print ("It is the way you wanted")
+else:
+      print ("It is not the way you wanted")
+
+    
+    
